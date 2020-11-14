e4a4b3b1ee9decd60bd9f76759da4841c77a0f0c
JadsonJose <jadsonjosesilva@gmail.com>
2020-11-14 20:28:00 -0300

    SINC

---
+++ config/constantes.php
@@ -0,0 +1,12 @@
+<?php
+return [
+
+    // Mail
+    'MAIL_TRANSPORT'   => '',
+    'MAIL_HOST'        => '',
+    'MAIL_PORT'        => 465,
+    'MAIL_ENCRYPTION'  => 'ssl',
+    'MAIL_USERNAME'    => '',
+    'MAIL_FROM'        => '',
+    'MAIL_FROM_NAME'   => '',
+];
